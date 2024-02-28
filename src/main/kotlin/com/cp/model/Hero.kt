a2945901f78e9e6f6b96c04d7a56d5522521bd7e
CPeeee <cphan1363@gmail.com>
2024-02-27 23:09:45 -0800

    Create Model Classes

---
+++ src/main/kotlin/com/cp/model/Hero.kt
@@ -0,0 +1,18 @@
+package com.cp.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hero(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val birthMonth: String,
+    val birthDay: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>
+)
